c8eb623d0bf12b7219a9d0e57d5744e8e11e27fa
ionutbv16 <ionu_ess@yahoo.com>
2018-04-09 21:11:07 +0100

    Add files via upload

--- react-apollo-client/src/reducers/persons.js
+++ react-apollo-client/src/reducers/persons.js
@@ -1,10 +1,10 @@
 
-import { SET_RESTAURANTS } from '../actions/actions_persons.js';
+import { SET_PERSONS } from '../actions/actions_persons.js';
 
 export default function persons(state = [], action = {}) {
   switch(action.type) {
    
-    case SET_RESTAURANTS:
+    case SET_PERSONS:
       return action.persons;
     default: return state;
   }
